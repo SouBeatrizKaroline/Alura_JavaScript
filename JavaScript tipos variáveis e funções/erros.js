fe5947721f10b45e56532f4bee29fbb15c2011fd
Beatriz0Karoline <81595085+Beatriz0karoline@users.noreply.github.com>
2023-02-22 11:47:52 -0300

    Main erros js

---
+++ JavaScript tipos variáveis e funções/erros.js
@@ -0,0 +1,14 @@
+// erro por falta de definição de valor em const
+// const numero;
+
+const numero = 0;
+const minhaVar = "oi";
+
+// erro de sintaxe
+//console.log(numero
+
+//erro de referencia
+// console.log(suaVar)
+
+console.log(numero)
+console.log(minhaVar)
---
+++ JavaScript tipos variáveis e funções/script2.js
@@ -0,0 +1,19 @@
+// tipagem dinâmica
+// untyped
+// multiparadigma, mas pode ser usado com apenas um paradigma
+// É levemente baseado no Java, mas bem pouco mesmo
+// O nome do JavaScript é ECMAScript
+// Foi batizado com o nome de LiveScript
+// ES6 / ES2015 - Versão do JS que saiu em 2015
+// depois da versão de 2015, começou a se ter versões anuais
+// ES6 / ES2015 é a versão mais famosa
+// Linguagem interpretada
+
+let minhaVar = 567; //number
+minhaVar = "texto"; //string
+minhaVar = true; //booleano
+
+// NodeJS não é uma linguagem de programação, mas o interpretador de JavaScript
+// O Node trás consigo um console
+// NodeJS foi desenvolvido em 2010, para conseguir usar o JavaScript fora do navegador
+// Node é um ambiente de interpretação JavaScript
