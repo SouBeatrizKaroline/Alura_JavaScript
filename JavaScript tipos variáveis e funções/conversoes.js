015747a7bc3fd966c770c5161b02e9f07bde59e0
Beatriz0Karoline <81595085+Beatriz0karoline@users.noreply.github.com>
2023-02-22 11:47:07 -0300

    erros em js

--- JavaScript tipos variáveis e funções/conversoes.js
+++ JavaScript tipos variáveis e funções/conversoes.js
@@ -47,3 +47,76 @@ let usuarioConectado = false;
 console.log(Number(usuarioConectado)); // teremos a conversão da booleana para número, sendo que false (falso) retorna o número 0.
 usuarioConectado = true;
 console.log(Number(usuarioConectado)); // agora teremos a conversão de true (verdadeiro) para o número 1.
+
+//test6
+
+/*
+let cpfUsuario = "12312312312"
+
+let var = 0;
+let if = 0;
+let const = "Alura";
+
+let varInicial = 0;
+let ifFalse = 0;
+let constDeTexto = "Alura";
+*/
+
+/*
+arguments
+as
+async
+await
+break
+case
+catch
+class
+const
+continue
+debugger
+default
+delete
+do
+else
+eval
+export
+extends
+false
+finally
+for
+from
+function
+get
+if
+import
+in
+instanceof
+let
+of
+new
+null
+return
+set
+static
+super
+switch
+target
+this
+throw
+true
+try
+typeof
+var
+void
+while
+with
+yield
+
+enum
+implements
+interface
+package
+private
+protected
+public
+*/
