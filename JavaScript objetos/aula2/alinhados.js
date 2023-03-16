675a581659cd067ab7a72ea3e9f789174aead4c8
Beatriz0Karoline <81595085+Beatriz0karoline@users.noreply.github.com>
2023-03-15 23:17:43 -0300

    Main

---
+++ JavaScript objetos/aula2/alinhados.js
@@ -0,0 +1,18 @@
+const cliente = {
+    nome: "Joao",
+    idade: 24,
+    email: "joao@firma.com",
+    telefone: ['11555555550', '1144444440'],
+};
+
+cliente.endereco = {
+    rua: "R. Joseph Climber",
+    numero: 1337,
+    apartamento: true,
+    complemento: "ap 934",
+};
+
+// console.log(cliente);
+// console.log(cliente["endereco"]);
+
+console.log(cliente.endereco);
---
+++ JavaScript objetos/aula2/tipos.js
@@ -0,0 +1,9 @@
+const cliente = {
+    nome: "Joao",
+    idade: 24,
+    email: "joao@firma.com",
+    telefone: ['11555555550', '1144444440'],
+    //telefone2: "1144444440",
+};
+
+console.log(cliente.telefone);
